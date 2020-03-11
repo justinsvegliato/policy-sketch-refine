8588e166069ac9c950352e46e8d793a586d8266b
Justin Svegliato <justin.svegliato@gmail.com>
2020-03-11 18:23:30 -0400

    Ported over MDP code from JavaScript (needs to be debugged, refactored, and commented)

---
+++ src/example.py
@@ -0,0 +1,19 @@
+import grid_world_mdp
+import printer
+
+
+def main():
+    mdp = grid_world_mdp.GridWorldMdp([
+        ['O', 'O', 'W', 'W', 'O', 'O', 'O', 'W', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'W', 'W', 'O', 'W', 'O', 'W', 'O', 'W', 'O', 'O'],
+        ['O', 'O', 'W', 'W', 'O', 'W', 'O', 'O', 'O', 'W', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'W', 'W', 'W', 'W', 'W', 'O', 'O'],
+        ['O', 'O', 'W', 'W', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', 'W', 'W', 'W', 'W', 'W', 'G', 'O']
+    ])
+
+    printer.print_mdp(mdp)
+
+
+if __name__ == '__main__':
+    main()

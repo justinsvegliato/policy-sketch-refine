4850e553a5633aeccbb589a8f0d1498623fa0700
Justin Svegliato <justin.svegliato@gmail.com>
2020-03-13 05:28:43 -0400

    Hacked up the example code to print out the policy

--- src/example.py
+++ src/example.py
@@ -1,33 +1,42 @@
 from grid_world_mdp import GridWorldMDP
 from cplex_mdp import CplexMDP
+import printer
 
 
 def main():
-    grid_world_mdp = GridWorldMDP([
+    grid_world = [
         ['O', 'O', 'W', 'W', 'O', 'O', 'O', 'W', 'O', 'O', 'O', 'O'],
         ['O', 'O', 'W', 'W', 'O', 'W', 'O', 'W', 'O', 'W', 'O', 'O'],
         ['O', 'O', 'W', 'W', 'O', 'W', 'O', 'O', 'O', 'W', 'O', 'O'],
         ['O', 'O', 'O', 'O', 'O', 'W', 'W', 'W', 'W', 'W', 'O', 'O'],
         ['O', 'O', 'W', 'W', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O'],
         ['O', 'O', 'O', 'O', 'O', 'W', 'W', 'W', 'W', 'W', 'G', 'O']
-    ])
+    ]
+    grid_world_mdp = GridWorldMDP(grid_world)
 
-    print("Solving the grid world MDP...")
+    print('Solving the grid world MDP...')
     mdp = CplexMDP()
     mdp.load_mdp(grid_world_mdp)
     mdp.formulate_lp(gamma=0.9)
     mdp.solve_lp()
     s = mdp.get_solution(gamma=0.9)
 
-    print(s["objective_value"])
-    print(s["state_values"])
-    print(s["policy"])
+    print()
 
-    # print("Solving a random MDP...")
-    # mdp = CplexMDP()
-    # mdp.load_random_mdp(500, 2)
-    # mdp.formulate_lp(gamma=0.5)
-    # mdp.solve_lp()
+    print(s['objective_value'])
+    print(s['state_values'])
+    print(s['policy'])
+
+    print()
+
+    print('Printing the grid world domain...')
+    printer.print_grid_world_domain(grid_world)
+
+    print()
+
+    print('Printing the policy...')
+    canonical_policy = [grid_world_mdp.actions()[value] for value in s["policy"]]
+    printer.print_grid_world_policy(grid_world, canonical_policy)
 
 
 if __name__ == '__main__':

98c73c5282b0c8bf8b60c50f9b30fc2782a5e0f3
Justin Svegliato <justin.svegliato@gmail.com>
2020-03-24 14:18:17 -0400

    Simplified the memory MDP class because now some of it is in the module

--- src/memory_mdp.py
+++ src/memory_mdp.py
@@ -1,63 +1,25 @@
 import numpy as np
 
 
-class MemoryMDP(object):
-    def __init__(self):
-        self.n_states = None
-        self.n_actions = None
-        self.rewards = None
-        self.transition_probabilities = None
-        self.start_probabilities = None
+class MemoryMDP:
+    def __init__(self, mdp):
+        self.states = mdp.states()
+        self.actions = mdp.actions()
 
-    def load_mdp(self, mdp):
-        states = mdp.states()
-        actions = mdp.actions()
-
-        self.n_states = len(states)
-        self.n_actions = len(actions)
+        self.n_states = len(self.states)
+        self.n_actions = len(self.actions)
 
         self.rewards = np.zeros(shape=(self.n_states, self.n_actions))
-        for i in range(self.n_states):
-            for j in range(self.n_actions):
-                self.rewards[i, j] = mdp.reward_function(states[i], actions[j])
+        for state in range(self.n_states):
+            for action in range(self.n_actions):
+                self.rewards[state, action] = mdp.reward_function(self.states[state], self.actions[action])
 
         self.transition_probabilities = np.zeros(shape=(self.n_states, self.n_actions, self.n_states))
-        for i in range(self.n_states):
-            for j in range(self.n_actions):
-                for k in range(self.n_states):
-                    self.transition_probabilities[i, j, k] = mdp.transition_function(states[i], actions[j], states[k])
+        for state in range(self.n_states):
+            for action in range(self.n_actions):
+                for successorState in range(self.n_states):
+                    self.transition_probabilities[state, action, successorState] = mdp.transition_function(self.states[state], self.actions[action], self.states[self.successorState])
 
-        self.start_probabilities = np.zeros(self.n_states)
-        for i in range(self.n_states):
-            self.start_probabilities[i] = mdp.start_state_function(states[i])
-
-    def load_random_mdp(self, n_states, n_actions):
-        np.random.seed(0)
-
-        self.n_states = n_states
-        self.n_actions = n_actions
-
-        self.rewards = np.random.randint(0, 10, size=(self.n_states, self.n_actions))
-
-        self.transition_probabilities = np.empty(shape=(self.n_states, self.n_actions, self.n_states))
-        for i in range(self.n_states):
-            for j in range(self.n_actions):
-                self.transition_probabilities[i, j] = np.random.dirichlet(np.ones(self.n_states), size=1)[0]
-
-        self.start_probabilities = np.random.dirichlet(np.ones(self.n_states), size=1)[0]
-
-    def formulate_lp(self):
-        assert self.n_states is not None
-        assert self.n_actions is not None
-        assert self.rewards is not None
-        assert self.transition_probabilities is not None
-        assert self.start_probabilities is not None
-        assert self.rewards.shape == (self.n_states, self.n_actions)
-        assert self.transition_probabilities.shape == (self.n_states, self.n_actions, self.n_states)
-        assert self.start_probabilities.shape == (self.n_states,)
-
-    def solve_lp(self):
-        raise NotImplementedError
-
-    def get_solution(self):
-        raise NotImplementedError
+        self.start_state_probabilities = np.zeros(self.n_states)
+        for state in range(self.n_states):
+            self.start_state_probabilities[state] = mdp.start_state_function(self.states[state])

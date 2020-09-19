0bc856f3e1eb120f5b372ded31133d9434666e94
Samer Nashed <samernashed@gmail.com>
2020-09-19 14:33:25 -0400

    I bet one pizza on this mdp

--- src/reduced_earth_observation_mdp.py
+++ src/reduced_earth_observation_mdp.py
@@ -14,13 +14,16 @@ ACTION_DETAILS = {
     },
 }
 
+PROB_WEATHER_GETS_WORSE = 0.1
+PROB_WEATHER_GETS_BETTER = 0.1
+PROB_WEATHER_STAYS_SAME = 0.8
+
 MIN_VISIBILITY = 0
 MAX_VISIBILITY = 2
 
 DEFAULT_NUM_POI = 5
 
-# TODO: allow single dict to specify everything?
-class EarthObservationMDP:
+class ReducedEarthObservationMDP:
     def __init__(self, size=None, points_of_interest=None, visibility=None):
         if size = None:
             self.size = (12, 12)
@@ -50,7 +53,7 @@ class EarthObservationMDP:
             self.__initRandomVisibility()
         elif isinstance(visibility, int):
             self.__initConstantVisibility(visibility)
-        elif isinstance(visibility, list):
+        elif isinstance(visibility, dict):
             self.__initExactVisibility(visibility)
         else:
             print("Visibility arg not parsed correctly")
@@ -67,33 +70,32 @@ class EarthObservationMDP:
         for point in points_of_interest:
             self.poi_description[point] = 0
 
-    
-    
-    def __initVisibility(self):
+    def __initRandomVisibility(self):
         # Random initialization
-        self.visibility = np.empty([self.size, self.size], dtype=int)
-        for x in range(self.size):
-            for y in range(self.size):
-                self.visibility[x][y] = randint(0, 10)
-
-
-
+        for point in self.poi_description:
+            rand_vis = randint(MIN_VISIBILITY, MAX_VISIBILITY)
+            self.poi_description[point] = rand_vis
+    
+    def __initConstantVisibility(self, visibility):
+        # Uniform visibility
+        visibility = max(MIN_VISIBILITY, visibility)
+        visibility = min(MAX_VISIBILITY, visibility)
+        for point in self.poi_description:
+            self.poi_description[point] = visibility
+    
+    def __initExactVisibility(self, visibility):
+        self.poi_description = visibility
 
     def __weatherEvolution(self):
-
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        # Not sure if we need this....
+        placeholder = 1
         
     def states(self):
-        return list(range()) #TODO: better way to do this
+        locations = self.size[0] * self.size[1]
+        base = (MAX_VISIBILITY - MIN_VISIBILITY + 1)
+        power = self.num_points_of_interest
+        total_number_of_states = pow(base, power) * locations
+        return list(range(total_number_of_states)) 
 
     def actions(self):
         return list(ACTION_DETAILS.keys())
@@ -101,7 +103,7 @@ class EarthObservationMDP:
     def transition_function(self, state, action, successor_state):
 
         # Periodic boundaries for East-West direction
-        if state.col == self.size - 1 and successor_state.col != 0:
+        if state.col == self.size[1] - 1 and successor_state.col != 0:
             return 0
         
         # We always move East by one grid cell
@@ -128,44 +130,44 @@ class EarthObservationMDP:
         if ACTION_DETAILS[action] == 'NORTH' and state.row != successor_state.row - 1:
             return 0
 
+        weather_transition_prob = 1.0
+        for loc in state.poi_weather:
+            current_loc_weather = state.poi_weather[loc]
+            new_loc_weather = successor_state.poi_weather[loc]
 
-        # TODO: take into account boundary conditions for satellite pos and weather limits...
-        #       try to limit weather horizon
-        # Weather model: %10 chance +/- 2 vis (5% each), 20% +/- 1 vis (10% each), 70% chance no change
-        weather_diff = np.absolute(state.visibility - successor_state.visibility)
-        if np.any(weather_diff > 2): 
-            return 0
-        else:
-            big_change = np.count_nonzero(weather_diff == 2)
-            small_change = np.count_nonzero(weather_diff == 1)
-            no_change = np.count_nonzero(weather_diff == 0)
-            #NOTE: I'm concerned that this number will be far too small - it's maximum value is 0.7^(10,000), or (NO_WEATHER_PROB ^ (size * size)).
-            #NOTE: on the bright side, it's possible using sketch refine will fix this problem to some degree since the probabilities 
-            #      in the abstract MDP's transition function should be much higher
-            #NOTE: One possible solution is to limit the weather updates to some subset of the "world" relative to the satellite,
-            #      say, any cell reachable within k timesteps. This would give roughly k(k+1) + k as the exponent, which is hopefully << 10,000
-            return pow(BIG_WEATHER_PROB, big_change) * pow(SMALL_WEATHER_PROB, small_change) * pow(NO_WEATHER_PROB, no_change)
+            assert (current_loc_weather >= MIN_VISIBILITY and current_loc_weather <= MAX_VISIBILITY),"Bad weather value in current state"
+            assert (new_loc_weather >= MIN_VISIBILITY and new_loc_weather <= MAX_VISIBILITY),"Bad weather value in successor state"
 
+            # Weather in our model cannot change from good to bad immediately
+            if abs(new_loc_weather - current_loc_weather) > 1:
+                return 0
 
-        print("I reached the end and didn't find a return... something is broken in EarthObservationMDP transition function.")
-        return 0
-
+            elif current_loc_weather == MIN_VISIBILITY:
+                # Weather cannot get worse than minimum visibility
+                if new_loc_weather == current_loc_weather:
+                    weather_transition_prob *= (PROB_WEATHER_GETS_WORSE + PROB_WEATHER_STAYS_SAME)
+                else:
+                    weather_transition_prob *= PROB_WEATHER_GETS_BETTER
+            
+            elif current_loc_weather == MAX_VISIBILITY:
+                # Weather cannot get better than maximum visibility
+                if new_loc_weather == current_loc_weather:
+                    weather_transition_prob *= (PROB_WEATHER_GETS_BETTER + PROB_WEATHER_STAYS_SAME)
+                else:
+                    weather_transition_prob *= PROB_WEATHER_GETS_WORSE
+                
+            else:
+                if new_loc_weather == current_loc_weather:
+                    weather_transition_prob *= PROB_WEATHER_STAYS_SAME
+                elif new_loc_weather > current_loc_weather:
+                    weather_transition_prob *= PROB_WEATHER_GETS_BETTER
+                else:
+                    weather_transition_prob *= PROB_WEATHER_GETS_WORSE
+                
     def reward_function(self, state, action):
+        if state.loc in state.poi_weather and ACTION_DETAILS[action] == 'IMAGE':
+            return 1.0 + 1.0 * state.poi_weather[state.loc]
+        return -0.01
 
-        if state.is_poi[state.row][state.col] and ACTION_DETAILS[action] == 'IMAGE':
-            return 0.1 * state.visibility[state.row][state.col] 
-        elif not state.is_poi[state.row][state.col] and ACTION_DETAILS[action] == 'IMAGE':
-            return -0.01
-
-        return 0
-
-    #TODO: need to update this so it works with state representation
     def start_state_function(self, state):
-        start_states = []
-
-        for row in range(self.height):
-            for column in range(self.width):
-                if self.grid_world[row][column] != 'W':
-                    start_states.append(self.width * row + column)
-
-        return 1.0 / len(start_states) if state in start_states else 0
+        return 1.0 / len(self.states()) 

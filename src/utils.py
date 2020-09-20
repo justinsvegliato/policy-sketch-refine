18ffc7f9b35475fcbf39f543450c1d29c8031a83
Justin Svegliato <justin.svegliato@gmail.com>
2020-09-20 17:53:23 -0400

    Changed the symbol

--- src/utils.py
+++ src/utils.py
@@ -90,7 +90,7 @@ def print_progress(count, total, suffix=''):
     filled_len = int(round(bar_len * count / float(total)))
 
     percents = round(100.0 * count / float(total), 1)
-    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    bar = '#' * filled_len + '-' * (bar_len - filled_len)
 
     sys.stdout.write('[%s] %s%s %s\r' % (bar, percents, '%', suffix))
     sys.stdout.flush()

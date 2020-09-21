eb54009e7895029cade527d26eac47ccb42596b5
Justin Svegliato <justin.svegliato@gmail.com>
2020-09-21 00:28:56 -0400

    Deleted a function (may get back later)

--- src/printer.py
+++ src/printer.py
@@ -83,32 +83,6 @@ def print_mdp(mdp):
     print_start_state_function(mdp)
 
 
-def print_grid_world_domain(grid_world, current_state):
-    height = len(grid_world)
-    width = len(grid_world[0])
-
-    current_row = math.floor(current_state / width)
-    current_column = current_state - current_row * width
-
-    for row in range(height):
-        text = ""
-
-        for column in range(width):
-            if row == current_row and column == current_column:
-                text += "R"
-            elif grid_world[row][column] == 'W':
-                text += "\u25A0"
-            elif grid_world[row][column] == 'G':
-                text += "\u272A"
-            elif grid_world[row][column] == 'S':
-                text += "\u229B"
-            else:
-                text += "\u25A1"
-            text += "  "
-
-        print(f"{text}")
-
-
 def print_grid_world_policy(grid_world, policy, visited_states=[], expanded_states={}):
     SYMBOLS = {
         'STAY': '\u2205',

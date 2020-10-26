9c9513692af7fda8051a6feeda3cec3bab5c0dd0
Matteo Brucato <mattfeel@gmail.com>
2020-10-26 18:23:10 +0100

    Small change to script.

--- src/yaml_to_json.sh
+++ src/yaml_to_json.sh
@@ -4,7 +4,8 @@
 find . -name "*.yaml" | while read file
 do
   filename=$(basename -- "$file")
-  mkdir -p "json/$(dirname "$file")"
-  json_name="json/$(dirname "$file")/${filename%.*}.json"
+#  mkdir -p "json/$(dirname "$file")"
+  json_name="$(dirname "$file")/${filename%.*}.json"
   yq . "$file" > "$json_name"
+  echo "$file --> $json_name"
 done
